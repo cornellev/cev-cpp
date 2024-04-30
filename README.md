e295f1c6c44ac01e8c82162ce9b6d854b34f30c2
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2024-04-30 19:52:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# cev-c++
+
+This repository contains information to setup a C++ project for CEV.
+
+```shell
+chmod u+x ./setup.sh
+./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,1 @@
+wget https://gist.githubusercontent.com/ethanuppal/e841afafbb1f33327cfbebb3a6a3ce8b/raw/eb1c5bd022b8e8105e66128fd7086bc111982fea/.clang-format
