69c8bd7985693ef86fc36fbf9e2cd91ebe5631fa
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2024-04-30 19:56:32 -0400

    Update instructions

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # cev-c++
 
 This repository contains information to setup a C++ project for CEV.
+Run the following commands and follow printed instructions.
 
 ```shell
 chmod u+x ./setup.sh
--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
+echo "==> CEV C++ setup wizard"
+
 wget https://gist.githubusercontent.com/ethanuppal/e841afafbb1f33327cfbebb3a6a3ce8b/raw/eb1c5bd022b8e8105e66128fd7086bc111982fea/.clang-format
+
+echo "==> Please add formatting via the clang format file in the current directory to your VSCode settings"
